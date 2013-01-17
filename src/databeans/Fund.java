d71408e32d519dd321d51ecdaa78e8ce4d2caa71
Yuan Cao <mail@caoyuan.me>
2013-01-17 16:57:01 -0500

    databeans: Fund and History
    Signed-off-by: Yuan Cao <mail@caoyuan.me>

--- src/databeans/Fund.java
+++ src/databeans/Fund.java
@@ -1,5 +1,34 @@
 package databeans;
 
 public class Fund {
-
+	private int id = -1; // fund_id, primary key
+	private String name = null; // the full name for the fund
+	private String symbol = null; // the short name for the fund
+	
+	public int compareTo(Fund other) {
+		// Order first by lastName and then by firstName
+		int c = name.compareTo(other.name);
+		if (c != 0) return c;
+		return symbol.compareTo(other.symbol);
+	}
+	
+	public boolean equals(Object obj) {
+		if (obj instanceof Fund) {
+			Fund other = (Fund) obj;
+			return name.equals(other.name) || symbol.equals(other.symbol);
+		}
+		return false;
+	}
+	
+	public String getName()      { return name;       }
+	public String getSymbol()    { return symbol;     }
+	public int    getId()        { return id;         }
+	
+	public void setName(String a)    { name = a;   }
+	public void setSymbol(String a)  { symbol = a; }
+	public void setId(int a)         { id = a;     }
+	
+	public String toString() {
+		return "Fund("+getName()+","+getSymbol()+")";
+	}
 }
--- src/databeans/History.java
+++ src/databeans/History.java
@@ -1,5 +1,19 @@
 package databeans;
 
-public class History {
+import java.util.Date;
 
+public class History {
+	
+	private int id; //fund_id
+	private Date date; //price date
+	private double price; //daily price
+	
+	public int getId()       { return id;    }
+	public Date getDate()    { return date;  }
+	public double getPrice() { return price; }
+	
+	public void setId(int a)       { id = a;       }
+	public void setDate(Date a)    { date = a;     }
+	public void setPrice(double a) { price = a;    }
+	
 }
