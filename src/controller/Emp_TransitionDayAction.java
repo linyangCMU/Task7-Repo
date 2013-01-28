398791643854266d43f1471e1231fd2109e257f2
Yuan Cao <mail@caoyuan.me>
2013-01-28 17:21:22 -0500

    fix a bug in HistoryDAO::create() transactionday can write new data in history table.
    Signed-off-by: Yuan Cao <mail@caoyuan.me>

--- src/controller/Emp_TransitionDayAction.java
+++ src/controller/Emp_TransitionDayAction.java
@@ -1,11 +1,13 @@
 package controller;
 
 import java.sql.Date;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
+import javax.swing.text.SimpleAttributeSet;
 
 import org.mybeans.form.FormBeanException;
 import org.mybeans.form.FormBeanFactory;
@@ -13,6 +15,7 @@ import org.mybeans.form.FormBeanFactory;
 import databeans.Customer;
 import databeans.Employee;
 import databeans.Fund;
+import databeans.History;
 import databeans.Portfolio;
 import databeans.Position;
 import databeans.Transaction;
@@ -52,6 +55,7 @@ public class Emp_TransitionDayAction extends Action {
         
         try {
             
+            //If employee is not logged in, direct him/her to the employee login page.
             Employee employee = (Employee) request.getSession(false).getAttribute("employee");
             if(employee == null) {
                 return "login-emp.jsp";
@@ -61,29 +65,46 @@ public class Emp_TransitionDayAction extends Action {
             Emp_TransitionDayForm form = new Emp_TransitionDayForm(request);
             request.setAttribute("form",form);
             
-            //get fund info:
-            ArrayList<Fund> funds = fundDAO.lookup(".");
-            for (Fund fund : funds) {
-                
-                fund.setPrice(historyDAO.lookupLatestPriceAndDate(fund.getId(), new Date(0)));
-                System.out.println(fund.getName() + ":");
-            }
-            
-            request.setAttribute("funds", funds);
-            
             // if the transition day form is not ready:
             // present the form
             if (!form.isPresent()) {
+              //get fund info:
+                ArrayList<Fund> funds = fundDAO.lookup(".");
+                Date date = new Date(0);
+                for (Fund fund : funds) {
+                    fund.setPrice(historyDAO.lookupLatestPriceAndDate(fund.getId(), date));
+                }
+                date.setTime(date.getTime() + 86400000);
+                SimpleDateFormat format = new SimpleDateFormat("MM/dd/yyyy");
+                request.setAttribute("date", format.format(date));
+                request.setAttribute("funds", funds);
+                
                 return "transition-day-emp.jsp";
             }
-
+            
+            //Validate the form
             // Any validation errors?
             errors.addAll(form.getValidationErrors());
             if (errors.size() != 0) {
                 return "transition-day-emp.jsp";
             }
             
-            // update the fund history database with new fund prices
+            //get form info:
+            Date transitionDay = new Date(form.getDate().getTime());
+            ArrayList<Integer> fundIds = form.getFundIds();
+            ArrayList<Double> prices = form.getPrices();
+            
+            //update the funds' prices with newly provided prices
+            History history = new History();
+            for (int i=0; i<fundIds.size(); i++) {
+                int id = fundIds.get(i);
+                double price = prices.get(i);
+                history.setDate(transitionDay);
+                history.setId(id);
+                history.setPrice(price);
+                historyDAO.create(history);
+            }
+            
             
             
             // Traverse the Transaction History table
